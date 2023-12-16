f02a351c0b7024404d6283d834797ebd34acfa1a
sonyokpara <sony.okpara@yahoo.com>
2023-12-16 08:34:13 +0000

    Fix the sidebar links

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -13,7 +13,7 @@
         <ul class="nav">
         <li class="nav-item nav-category">Main</li>
         <li class="nav-item">
-            <a href="dashboard.html" class="nav-link">
+            <a href="{{route('admin.dashboard')}}" class="nav-link">
             <i class="link-icon" data-feather="box"></i>
             <span class="link-title">Dashboard</span>
             </a>
@@ -28,25 +28,25 @@
             <div class="collapse" id="emails">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/email/inbox.html" class="nav-link">Inbox</a>
+                <a href="{{asset('pages/email/inbox.html')}}" class="nav-link">Inbox</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/email/read.html" class="nav-link">Read</a>
+                <a href="{{asset('pages/email/read.html')}}" class="nav-link">Read</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/email/compose.html" class="nav-link">Compose</a>
+                <a href="{{asset('pages/email/compose.html')}}" class="nav-link">Compose</a>
                 </li>
             </ul>
             </div>
         </li>
         <li class="nav-item">
-            <a href="pages/apps/chat.html" class="nav-link">
+            <a href="{{asset('pages/apps/chat.html')}}" class="nav-link">
             <i class="link-icon" data-feather="message-square"></i>
             <span class="link-title">Chat</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="pages/apps/calendar.html" class="nav-link">
+            <a href="{{asset('pages/apps/calendar.html')}}" class="nav-link">
             <i class="link-icon" data-feather="calendar"></i>
             <span class="link-title">Calendar</span>
             </a>
@@ -61,73 +61,73 @@
             <div class="collapse" id="uiComponents">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/ui-components/accordion.html" class="nav-link">Accordion</a>
+                <a href="{{asset('pages/ui-components/accordion.html')}}" class="nav-link">Accordion</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/alerts.html" class="nav-link">Alerts</a>
+                <a href="{{asset('pages/ui-components/alerts.html')}}" class="nav-link">Alerts</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/badges.html" class="nav-link">Badges</a>
+                <a href="{{asset('pages/ui-components/badges.html')}}" class="nav-link">Badges</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/breadcrumbs.html" class="nav-link">Breadcrumbs</a>
+                <a href="{{asset('pages/ui-components/breadcrumbs.html')}}" class="nav-link">Breadcrumbs</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/buttons.html" class="nav-link">Buttons</a>
+                <a href="{{asset('pages/ui-components/buttons.html')}}" class="nav-link">Buttons</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/button-group.html" class="nav-link">Button group</a>
+                <a href="{{asset('pages/ui-components/button-group.html')}}" class="nav-link">Button group</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/cards.html" class="nav-link">Cards</a>
+                <a href="{{asset('pages/ui-components/cards.html')}}" class="nav-link">Cards</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/carousel.html" class="nav-link">Carousel</a>
+                <a href="{{asset('pages/ui-components/carousel.html')}}" class="nav-link">Carousel</a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/ui-components/collapse.html" class="nav-link">Collapse</a>
+                    <a href="{{asset('pages/ui-components/collapse.html')}}" class="nav-link">Collapse</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/dropdowns.html" class="nav-link">Dropdowns</a>
+                <a href="{{asset('pages/ui-components/dropdowns.html')}}" class="nav-link">Dropdowns</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/list-group.html" class="nav-link">List group</a>
+                <a href="{{asset('pages/ui-components/list-group.html')}}" class="nav-link">List group</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/media-object.html" class="nav-link">Media object</a>
+                <a href="{{asset('pages/ui-components/media-object.html')}}" class="nav-link">Media object</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/modal.html" class="nav-link">Modal</a>
+                <a href="{{asset('pages/ui-components/modal.html')}}" class="nav-link">Modal</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/navs.html" class="nav-link">Navs</a>
+                <a href="{{asset('pages/ui-components/navs.html')}}" class="nav-link">Navs</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/navbar.html" class="nav-link">Navbar</a>
+                <a href="{{asset('pages/ui-components/navbar.html')}}" class="nav-link">Navbar</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/pagination.html" class="nav-link">Pagination</a>
+                <a href="{{asset('pages/ui-components/pagination.html')}}" class="nav-link">Pagination</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/popover.html" class="nav-link">Popovers</a>
+                <a href="{{asset('pages/ui-components/popover.html')}}" class="nav-link">Popovers</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/progress.html" class="nav-link">Progress</a>
+                <a href="{{asset('pages/ui-components/progress.html')}}" class="nav-link">Progress</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/scrollbar.html" class="nav-link">Scrollbar</a>
+                <a href="{{asset('')}}pages/ui-components/scrollbar.html" class="nav-link">Scrollbar</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/scrollspy.html" class="nav-link">Scrollspy</a>
+                <a href="{{asset('pages/ui-components/scrollspy.html')}}" class="nav-link">Scrollspy</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/spinners.html" class="nav-link">Spinners</a>
+                <a href="{{asset('pages/ui-components/spinners.html')}}" class="nav-link">Spinners</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/tabs.html" class="nav-link">Tabs</a>
+                <a href="{{asset('pages/ui-components/tabs.html')}}" class="nav-link">Tabs</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/ui-components/tooltips.html" class="nav-link">Tooltips</a>
+                <a href="{{asset('pages/ui-components/tooltips.html')}}" class="nav-link">Tooltips</a>
                 </li>
             </ul>
             </div>
@@ -141,16 +141,16 @@
             <div class="collapse" id="advancedUI">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/advanced-ui/cropper.html" class="nav-link">Cropper</a>
+                <a href="{{asset('pages/advanced-ui/cropper.html')}}" class="nav-link">Cropper</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/advanced-ui/owl-carousel.html" class="nav-link">Owl carousel</a>
+                <a href="{{asset('pages/advanced-ui/owl-carousel.html')}}" class="nav-link">Owl carousel</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/advanced-ui/sortablejs.html" class="nav-link">SortableJs</a>
+                <a href="{{asset('pages/advanced-ui/sortablejs.html')}}" class="nav-link">SortableJs</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/advanced-ui/sweet-alert.html" class="nav-link">Sweet Alert</a>
+                <a href="{{asset('pages/advanced-ui/sweet-alert.html')}}" class="nav-link">Sweet Alert</a>
                 </li>
             </ul>
             </div>
@@ -164,16 +164,16 @@
             <div class="collapse" id="forms">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/forms/basic-elements.html" class="nav-link">Basic Elements</a>
+                <a href="{{asset('pages/forms/basic-elements.html')}}" class="nav-link">Basic Elements</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/forms/advanced-elements.html" class="nav-link">Advanced Elements</a>
+                <a href="{{asset('pages/forms/advanced-elements.html')}}" class="nav-link">Advanced Elements</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/forms/editors.html" class="nav-link">Editors</a>
+                <a href="{{asset('pages/forms/editors.html')}}" class="nav-link">Editors</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/forms/wizard.html" class="nav-link">Wizard</a>
+                <a href="{{asset('pages/forms/wizard.html')}}" class="nav-link">Wizard</a>
                 </li>
             </ul>
             </div>
@@ -187,19 +187,19 @@
             <div class="collapse" id="charts">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/charts/apex.html" class="nav-link">Apex</a>
+                <a href="{{asset('pages/charts/apex.html')}}" class="nav-link">Apex</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/charts/chartjs.html" class="nav-link">ChartJs</a>
+                <a href="{{asset('pages/charts/chartjs.html')}}" class="nav-link">ChartJs</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/charts/flot.html" class="nav-link">Flot</a>
+                <a href="{{asset('pages/charts/flot.html')}}" class="nav-link">Flot</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/charts/peity.html" class="nav-link">Peity</a>
+                <a href="{{asset('pages/charts/peity.html')}}" class="nav-link">Peity</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/charts/sparkline.html" class="nav-link">Sparkline</a>
+                <a href="{{asset('pages/charts/sparkline.html')}}" class="nav-link">Sparkline</a>
                 </li>
             </ul>
             </div>
@@ -213,10 +213,10 @@
             <div class="collapse" id="tables">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/tables/basic-table.html" class="nav-link">Basic Tables</a>
+                <a href="{{asset('pages/tables/basic-table.html')}}" class="nav-link">Basic Tables</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/tables/data-table.html" class="nav-link">Data Table</a>
+                <a href="{{asset('pages/tables/data-table.html')}}" class="nav-link">Data Table</a>
                 </li>
             </ul>
             </div>
@@ -230,13 +230,13 @@
             <div class="collapse" id="icons">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/icons/feather-icons.html" class="nav-link">Feather Icons</a>
+                <a href="{{asset('pages/icons/feather-icons.html')}}" class="nav-link">Feather Icons</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/icons/flag-icons.html" class="nav-link">Flag Icons</a>
+                <a href="{{asset('pages/icons/flag-icons.html')}}" class="nav-link">Flag Icons</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/icons/mdi-icons.html" class="nav-link">Mdi Icons</a>
+                <a href="{{asset('pages/icons/mdi-icons.html')}}" class="nav-link">Mdi Icons</a>
                 </li>
             </ul>
             </div>
@@ -251,22 +251,22 @@
             <div class="collapse" id="general-pages">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/general/blank-page.html" class="nav-link">Blank page</a>
+                <a href="{{asset('pages/general/blank-page.html')}}" class="nav-link">Blank page</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/general/faq.html" class="nav-link">Faq</a>
+                <a href="{{asset('pages/general/faq.html')}}" class="nav-link">Faq</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/general/invoice.html" class="nav-link">Invoice</a>
+                <a href="{{asset('pages/general/invoice.html')}}" class="nav-link">Invoice</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/general/profile.html" class="nav-link">Profile</a>
+                <a href="{{asset('pages/general/profile.html')}}" class="nav-link">Profile</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/general/pricing.html" class="nav-link">Pricing</a>
+                <a href="{{asset('pages/general/pricing.html')}}" class="nav-link">Pricing</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/general/timeline.html" class="nav-link">Timeline</a>
+                <a href="{{asset('pages/general/timeline.html')}}" class="nav-link">Timeline</a>
                 </li>
             </ul>
             </div>
@@ -280,10 +280,10 @@
             <div class="collapse" id="authPages">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/auth/login.html" class="nav-link">Login</a>
+                <a href="{{asset('pages/auth/login.html')}}" class="nav-link">Login</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/auth/register.html" class="nav-link">Register</a>
+                <a href="{{asset('pages/auth/register.html')}}" class="nav-link">Register</a>
                 </li>
             </ul>
             </div>
@@ -297,10 +297,10 @@
             <div class="collapse" id="errorPages">
             <ul class="nav sub-menu">
                 <li class="nav-item">
-                <a href="pages/error/404.html" class="nav-link">404</a>
+                <a href="{{asset('pages/error/404.html')}}" class="nav-link">404</a>
                 </li>
                 <li class="nav-item">
-                <a href="pages/error/500.html" class="nav-link">500</a>
+                <a href="{{asset('pages/error/500.html')}}" class="nav-link">500</a>
                 </li>
             </ul>
             </div>
