eb66b587fcfdc055197073bd2724d1a8613b36de
sonyokpara <sony.okpara@yahoo.com>
2023-12-16 10:04:13 +0000

    Add user  authentication

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AdminController extends Controller
 {
     public function dashboard(){
         return view('admin.dashboard');
+    }
+    
+     /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+     /**
+     * Handle an incoming authentication request.
+     */
+    public function login(LoginRequest $request){
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        $url = 'admin/dashboard';
+
+        return redirect()->intended($url);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function logout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,8 +35,13 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('admin/logout', [AdminController::class, 'logout'])
+                ->name('admin.logout');
 });
 
 Route::middleware(['auth', 'role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'dashboard'])->name('agent.dashboard');
 });
+
+Route::get('admin/login', [AdminController::class, 'create']);
+Route::post('admin/login', [AdminController::class, 'login'])->name('admin.login');
